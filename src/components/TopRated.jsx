06afe3d550d5f5fa2ef341d2178db084a0583e7a
Prashant Pal <prashantpal2024@gmail.com>
2025-05-29 19:37:39 +0530

    ok

--- src/components/TopRated.jsx
+++ src/components/TopRated.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -7,97 +8,71 @@ import 'swiper/css/pagination';
 // import required modules
 import { Navigation, Pagination } from 'swiper/modules';
 
-const TopRated = () => {
+const Nowplaying = () => {
+   const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+  
+    const token = import.meta.env.VITE_TMDB_TOKEN;
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+  
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  
+    useEffect(() => {
+      fetch(url, options)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Failed to fetch data');
+          }
+          return res.json();
+        })
+        .then((json) => setMovies(json.results))
+        .catch((err) => setError(err.message));
+    }, []);
+  
+    if (error) return <div>Error: {error}</div>;
   return (
      <div className='popular'>
-          <h2 style={{ paddingLeft:'10px', color: 'white',marginBottom:'20px' }}>Top Rated</h2>
-         <Swiper
-            modules={[Navigation, Pagination]}
-             simulateTouch={true} 
-          allowTouchMove={true}
-            spaceBetween={20}
-           slidesPerView={5}
-            navigation
-            pagination={{ clickable: true }}
-            style={{ paddingBottom: '40px' }} 
+           <h2 style={{ paddingLeft: '10px', color: 'white', marginBottom: '20px' }}>Top Rated</h2>
+           <Swiper
+             modules={[Navigation, Pagination]}
+             simulateTouch={true}
+             allowTouchMove={true}
+             spaceBetween={20}
+             navigation
+             pagination={{ clickable: true }}
+             style={{ paddingBottom: '40px' }}
              breakpoints={{
-    // when window width is >= 320px (mobile)
-    320: {
-      slidesPerView: 1,
-    },
-    // when window width is >= 768px (tablet)
-    768: {
-      slidesPerView: 3,
-    },
-    // when window width is >= 1024px (desktop)
-    1024: {
-      slidesPerView: 5,
-    },
-  }}  
-          >
-            
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="swiper-slide-card2">
-                <img src='/yFHHfHcUgGAxziP1C3lLt0q2T4s.webp' className="movieimage"/>
-                <div className="moviename">A MineCraft Movie</div>
-                <div className="moviedate">27 May 2025</div>
-              </div>
-            </SwiperSlide>
-           
-          </Swiper>
-    
-    
-       </div>
+               320: { slidesPerView: 1 },
+               768: { slidesPerView: 3 },
+               1024: { slidesPerView: 5 },
+             }}
+           >
+             {movies.map((movie) => (
+               <SwiperSlide key={movie.id}>
+                 <div className="swiper-slide-card2">
+                   <img
+                     className='movieimage'
+                     alt={movie.title}
+                     src={
+                       movie.poster_path
+                         ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+                         : '/fallback-image.jpg'
+                     }
+                   />
+                   <div className="moviename">{movie.title}</div>
+                   <div className="moviedate">{movie.release_date}</div>
+                 </div>
+               </SwiperSlide>
+             ))}
+           </Swiper>
+         </div>
   )
 }
 
-export default TopRated
+export default Nowplaying
