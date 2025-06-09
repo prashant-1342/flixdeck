83ecb6321d7e67f4bacaca60747320b6c924183f
Prashant Pal <prashantpal2024@gmail.com>
2025-06-10 00:50:30 +0530

    updated favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className=" text-white py-4 mt-auto">
       <div className="container text-center">
-        <p  className="mb-0">&copy; {new Date().getFullYear()} PlayerFire. All rights reserved.</p>
+        <p  className="mb-0">&copy; {new Date().getFullYear()} Asterisk. All rights reserved.</p>
         <div>
           <div>Developed by <a target='_blank' href='https://prashant-1342.github.io/My-Profile/' style={{color:'blueviolet',textDecoration:'none'}}>Prashant</a> </div>
         </div>
